17ad43a8a79d9a37cb9609958c50a054fe261f84
Jaiveer Bala <jaiveerbala@gmail.com>
2026-04-29 18:52:11 -0700

    pkce auth flow

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,8 +5,7 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY,
   {
     auth: {
-      flowType: 'pkce',
-      redirectTo: 'https://jaiveerbala.github.io/jvplanner'
+      flowType: 'pkce'
     }
   }
 )
